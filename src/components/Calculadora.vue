0f78fba5f1806ef2e1cc873df2815c2557945fb5
Leonardo Abril <leoabril98@gmail.com>
2024-02-16 23:35:04 -0300

    calculadora pronta

---
+++ src/components/Calculadora.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="calculadora">
+        <input type="number" v-model.number="numero1" @input="calcular" class="input-numero">
+        <select v-model="operacao" @change="calcular" class="select-operacao">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+        <input type="number" v-model.number="numero2" @input="calcular" class="input-numero">
+        <Display :resultado="resultado" />
+    </div>
+</template>
+  
+<script>
+import Display from './Display.vue';
+
+export default {
+    components: {
+        Display,
+    },
+    data() {
+        return {
+            numero1: 0,
+            numero2: 0,
+            operacao: '+',
+        };
+    },
+    computed: {
+        resultado() {
+            switch (this.operacao) {
+                case '+':
+                    return this.numero1 + this.numero2;
+                case '-':
+                    return this.numero1 - this.numero2;
+                case '*':
+                    return this.numero1 * this.numero2;
+                case '/':
+                    return this.numero1 / this.numero2;
+                default:
+                    return 0;
+            }
+        },
+    },
+    methods: {
+        calcular() {
+            // Nada a fazer aqui, pois os cálculos são realizados automaticamente através de v-model e @input
+        },
+    },
+};
+</script>
+  
+<style scoped>
+.calculadora {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.input-numero,
+.select-operacao {
+    margin: 5px;
+    padding: 5px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+}
+
+.input-numero:focus,
+.select-operacao:focus {
+    outline: none;
+}
+
+.display {
+    margin-top: 20px;
+    color: #fff;
+    background-color: #007bff;
+    padding: 10px;
+    border-radius: 5px;
+}
+</style>
+  
---
+++ src/components/Display.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="display">
+        <h2>Resultado: {{ resultado }}</h2>
+    </div>
+</template>
+  
+<script>
+export default {
+    props: {
+        resultado: Number,
+    },
+};
+</script>
+  
+<style scoped>
+.display {
+    margin-top: 20px;
+    color: #fff;
+    background-color: #007bff;
+    padding: 10px;
+    border-radius: 5px;
+}
+</style>
+  
